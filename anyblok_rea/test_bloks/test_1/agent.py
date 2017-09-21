36d7a853424cc7526df9f285162eea2ff72202b4
jssuzanne <jssuzanne@anybox.fr>
2017-09-21 18:51:54 +0200

    Add unit test

---
+++ anyblok_rea/test_bloks/test_1/agent.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# This file is a part of the AnyBlok project
+#
+#    Copyright (C) 2017 Simon ANDRE <sandre@anybox.fr>
+#    Copyright (C) 2017 Jean-Sebastien SUZANNE <jssuzanne@anybox.fr>
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file,You can
+# obtain one at http://mozilla.org/MPL/2.0/.
+from anyblok import Declarations
+from anyblok.column import Integer, String
+
+
+register = Declarations.register
+Agent = Declarations.Model.Agent
+
+
+@register(Agent)
+class Customer:
+    id = Integer(primary_key=True,
+                 foreign_key=Agent.use('id').options(ondelete='cascade'))
+    name = String(nullage=False)
---
+++ anyblok_rea/tests/test_agent.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# This file is a part of the AnyBlok project
+#
+#    Copyright (C) 2017 Simon ANDRE <sandre@anybox.fr>
+#    Copyright (C) 2017 Jean-Sebastien SUZANNE <jssuzanne@anybox.fr>
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file,You can
+# obtain one at http://mozilla.org/MPL/2.0/.
+from anyblok.tests.testcase import DBTestCase
+
+
+class TestAgent(DBTestCase):
+
+    blok_entry_points = ('bloks', 'test_bloks')
+
+    def test_install_test1(self):
+        registry = self.init_registry(None)
+        registry.upgrade(install=('rea_test_1',))
+        customer = registry.Agent.Customer.insert(name="My customer")
+        self.assertId(customer, registry.Entity.query().one())
