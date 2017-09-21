6f904eafe8ec8a2aca1fcb7d4cd65886b5466a39
jssuzanne <jssuzanne@anybox.fr>
2017-09-21 12:06:33 +0200

    Add rea blok

---
+++ anyblok_rea/rea/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# This file is a part of the AnyBlok project
+#
+#    Copyright (C) 2017 Simon ANDRE <sandre@anybox.fr>
+#    Copyright (C) 2017 Jean-Sebastien SUZANNE <jssuzanne@anybox.fr>
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file,You can
+# obtain one at http://mozilla.org/MPL/2.0/.
+from anyblok.blok import Blok
+from anyblok_rea.release import version
+from logging import getLogger
+logger = getLogger(__name__)
+
+
+class REABlok(Blok):
+    version = version
+    author = 'ANDRE Simon'
+
+    required = [
+        'anyblok-core',
+    ]
+
+    @classmethod
+    def import_declaration_module(cls):
+        pass
+
+    @classmethod
+    def reload_declaration_module(cls, reload):
+        pass
